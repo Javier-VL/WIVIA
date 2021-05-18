a57cc734d5abea08bb16613f4b16322be7398f79
Javier V <javierivanvelazquezlozano@gmail.com>
2021-05-17 21:38:00 -0500

    pasos asincronos de los motores

--- MECANISMO/mechanismV1.py
+++ MECANISMO/mechanismV1.py
@@ -3,6 +3,8 @@ import time
 import sys
  
 # Define the serial port and baud rate.
+#git pull origin main
+
 arduino = serial.Serial('COM3', 9600)
 
 
@@ -13,17 +15,17 @@ time.sleep(2)
 def moveOneStep():
     user_input = input("\n Derecha o Izquierda: ")
     
-    if user_input.upper =="D":
+    if user_input =="D":
         print(">")
         time.sleep(0.1) 
         arduino.write(b'D') 
         moveOneStep()
-    elif user_input.upper =="I":
+    elif user_input =="I":
         print("<")
         time.sleep(0.1) 
         arduino.write(b'I') 
         moveOneStep()
-    elif user_input.upper =="S":
+    elif user_input =="S":
         print("FINALIZADO")
         arduino.close()#CERRANDO PUERTO
     else:
